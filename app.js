05a6a47952d4fee1b80d4a1b4a4b4b567ede0559
Harsh Tiwari <tiwari.harsh1495@gmail.com>
2026-09-07 19:46:37 +0530

    Add click handlers to open drawer and close on backdrop click

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 let drawer = document.querySelector('.task-drawer');
 let backdrop = document.querySelector('.drawer-backdrop');
-
 let taskBtn = document.querySelectorAll('.add-task-btn');
+
+taskBtn.forEach(function (btn) {
+    btn.addEventListener('click', () => {
+        drawer.classList.add('open');
+        backdrop.classList.add('open');
+    })
+});
+
+backdrop.addEventListener('click', () => {
+    drawer.classList.remove('open');
+    backdrop.classList.remove('open');
+});
